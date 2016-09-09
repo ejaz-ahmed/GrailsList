50a5f52bcb0d7388f2383e402db0fd6fee2d51a3
Brandon Howard <brandon@bl-nk.net>
2016-09-09 05:38:37 -0500

    Added abbreviation for states and basic test data
    Using Bootstrap.groovy, we can run operations at application runtime to populate our databases or run any arbitrary code that is needed. In this case, I'm using the envrironments closure to only populate this test data when the application is running in the development environment: grails run-app

--- grails-app/domain/gcraigslist/area/State.groovy
+++ grails-app/domain/gcraigslist/area/State.groovy
@@ -2,9 +2,11 @@ package gcraigslist.area
 
 class State {
     String name
+    String abbreviation
 
     static hasMany = [cities:City]
 
     static constraints = {
+        abbreviation maxSize: 2
     }
 }
--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,19 @@
+import gcraigslist.area.City
+import gcraigslist.area.State
+
 class BootStrap {
 
     def init = { servletContext ->
+        environments{
+            development{
+                def cities = ['Midland','Odessa','Stanton','Big Spring']
+                State state = new State(name:"Texas",abbreviation:"TX")
+                cities.each {state.addToCities(new City(name: it))}
+
+                state.save(failOnError:true)
+
+            }
+        }
     }
     def destroy = {
     }
